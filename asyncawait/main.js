92976b85d1075854efa576fcba7cb3ce68dc2fe8
ethan <izawaryosuke0101@gmail.com>
2020-10-23 08:46:00 +0900

    asyncawait

---
+++ asyncawait/main.js
@@ -0,0 +1,23 @@
+//promiseをみやすくしたもの
+async function aTest1(){
+  return '非同期関数1';
+}
+console.log(aTest1().then((value)=>{console.log(value)}));
+
+//await:asyncの中でのみ使える
+
+function waiting(seconds){
+  return new Promise((ok)=>{{
+    setTimeout(()=>{
+      ok()
+    }, 1000*seconds)
+  }})
+};
+
+async function aTest2(){
+  console.log(1);
+  await waiting(2);
+  console.log(3);
+}
+
+aTest2();
